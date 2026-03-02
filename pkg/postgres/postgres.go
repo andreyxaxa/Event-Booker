ef72183f3d1799fae2823067247a60f0d13efaaa
Petrov Andrey <4ndrew1337@mail.ru>
2026-03-02 18:42:53 +0300

    feat: add postgres package

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,74 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/andreyxaxa/Event-Booker/pkg/logger"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	_defaultMaxPoolSize  = 1
+	_defaultConnAttempts = 10
+	_defaultConnTimeout  = time.Second
+)
+
+type Postgres struct {
+	maxPoolSize  int
+	connAttempts int
+	connTimeout  time.Duration
+
+	Builder squirrel.StatementBuilderType
+	Pool    *pgxpool.Pool
+
+	logger logger.Interface
+}
+
+func New(url string, l logger.Interface, opts ...Option) (*Postgres, error) {
+	pg := &Postgres{
+		maxPoolSize:  _defaultMaxPoolSize,
+		connAttempts: _defaultConnAttempts,
+		connTimeout:  _defaultConnTimeout,
+		logger:       l,
+	}
+
+	for _, opt := range opts {
+		opt(pg)
+	}
+
+	pg.Builder = squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+
+	poolConfig, err := pgxpool.ParseConfig(url)
+	if err != nil {
+		return nil, fmt.Errorf("postgres - New - pgxpool.ParseConfig: %w", err)
+	}
+
+	poolConfig.MaxConns = int32(pg.maxPoolSize) // nolint:gosec
+
+	for pg.connAttempts > 0 {
+		pg.Pool, err = pgxpool.NewWithConfig(context.Background(), poolConfig)
+		if err == nil {
+			break
+		}
+		l.Info("Postgres is trying to connect, attempts left: %d", pg.connAttempts)
+
+		time.Sleep(pg.connTimeout)
+
+		pg.connAttempts--
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("postgres - New - connAttempts == 0: %w", err)
+	}
+
+	return pg, nil
+}
+
+func (p *Postgres) Close() {
+	if p.Pool != nil {
+		p.Pool.Close()
+	}
+}
